3ca4aa6bda0874e6c3e8499ec4505c7f6b5d658b
Pinnochio23 <47636294+Pinnochio23@users.noreply.github.com>
2019-02-14 15:07:08 +0000

    Add files via upload

---
+++ bot 2019.py
@@ -0,0 +1,14 @@
+print("Hi my name is Claude, I am your amazing Bot")
+print("Ok, lets get started shall we ?")
+my_name="Kate"
+print("My name is ?"+ my_name)
+users_name=input("enter your name")
+print("Hello" + users_name + ", welcome")
+print("Ok, lets add some numbers")
+input1=input("Number 1>")
+input2=input("Number 2>")
+number1=int(input1)
+number2=int(input2)
+result=number1 + number2
+output=str (result)
+print(input1 + " + " + input2 + "=" +output)
